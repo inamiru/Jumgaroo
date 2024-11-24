5e6fad579c90514a74246bad7577c5ec0552a5b7
kenta yamada <gc.jewelfish@gmail.com>
2024-11-24 17:25:48 +0900

    new stage edit

--- Assets/Project/Scripts/Sound/BGMSoundManager.cs
+++ Assets/Project/Scripts/Sound/BGMSoundManager.cs
@@ -10,6 +10,7 @@ public class BGMSoundManager : MonoBehaviour
     public AudioClip titleBGM;   // タイトル画面用BGM
     public AudioClip stageSelectBGM;   // ステージ選択画面用BGM
     public AudioClip stage01BGM;   // ステージ01用BGM
+    public AudioClip stage02BGM;   // ステージ01用BGM
     public AudioClip resultBGM;   // リザルト用BGM
     public AudioClip gameclearBGM;   // ステージクリア用BGM
     public AudioClip gameoverBGM;   // ゲームオーバー用BGM
@@ -44,6 +45,7 @@ public class BGMSoundManager : MonoBehaviour
     public void PlayTitleBGM() => PlayBGM(titleBGM);
     public void PlayStageSelectBGM() => PlayBGM(stageSelectBGM);
     public void PlayStage01BGM() => PlayBGM(stage01BGM);
+    public void PlayStage02BGM() => PlayBGM(stage02BGM);
     public void PlayResultBGM() => PlayBGM(resultBGM);
     public void PlayGameClearBGM() => PlayBGM(gameclearBGM);
     public void PlayGameOverBGM() => PlayBGM(gameoverBGM);
